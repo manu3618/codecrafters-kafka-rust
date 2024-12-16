21244cff237118d1416098d8a286a926341f930b
manu3618 <manu+github@hbrt.eu>
2024-12-16 11:18:21 +0100

    Add mesage reading

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #![allow(unused_imports)]
 use anyhow::Result;
-use std::io::Write;
+use std::io::{Read, Write};
 use std::net::TcpListener;
 
 fn main() -> Result<()> {
@@ -13,11 +13,9 @@ fn main() -> Result<()> {
         match stream {
             Ok(mut stream) => {
                 println!("accepted new connection");
-                let m = Message {
-                    message_size: 0,
-                    header: MessageHeader { correlation_id: 7 },
-                    body: Vec::new(),
-                };
+                let mut buff = [0; 128];
+                stream.read(&mut buff)?;
+                let m = Message::from_bytes(&buff);
                 stream.write_all(&m.to_bytes())?;
                 stream.flush()?;
             }
@@ -38,10 +36,31 @@ struct Message {
 
 #[derive(Default, Clone, Debug)]
 struct MessageHeader {
-    correlation_id: i32,
+    request_api_key: i16,         //   The API key for the request
+    request_api_version: i16,     // The version of the API for the request
+    correlation_id: i32,          //A unique identifier for the request
+    client_id: String,            //  NULLABLE_STRING     The client ID for the request
+    tag_array: CompactBuffer<u8>, // Optional tagged fields
+}
+
+#[derive(Default, Clone, Debug)]
+struct CompactBuffer<T> {
+    array: Vec<T>,
 }
 
 impl Message {
+    fn from_bytes(input: &[u8]) -> Self {
+        Self {
+            message_size: i32::from_be_bytes(input[0..4].try_into().unwrap()),
+            header: MessageHeader {
+                request_api_key: i16::from_be_bytes(input[4..6].try_into().unwrap()),
+                request_api_version: i16::from_be_bytes(input[6..8].try_into().unwrap()),
+                correlation_id: i32::from_be_bytes(input[8..12].try_into().unwrap()),
+                ..Default::default()
+            },
+            ..Default::default()
+        }
+    }
     fn to_bytes(self) -> Vec<u8> {
         let mut m = Vec::new();
         m.extend_from_slice(&self.message_size.to_be_bytes());
@@ -56,5 +75,11 @@ impl MessageHeader {
         let mut m = Vec::new();
         m.extend_from_slice(&self.correlation_id.to_be_bytes());
         m
+    }
+}
+
+impl<T> CompactBuffer<T> {
+    fn to_bytes(self) -> Vec<u8> {
+        todo!()
     }
 }
